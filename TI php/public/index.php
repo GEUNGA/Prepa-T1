d201d92269ea82b2c35d6c93f3d81fa4a17d7392
GEUNGA <ungaro.geraldine@gmail.com>
2025-02-18 13:55:41 +0100

    creation de get

---
+++ TI php/public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_GET["p"])){
+    switch($_GET["p"]){
+        case "accueil":
+            include "../templates/accueilView.php";
+            break;
+        case "apropos":
+            include "../templates/aproposView.php";
+            break;
+        case "contact":
+            include "../templates/contactView.php";
+            break;
+        case "histoire":
+            include "../templates/histoireView.php";
+            break;
+        default:
+        include "../templates/erreur404View.php";
+            break;
+
+    }   
+        else{
+            include "../templates/accueilView.php";
+
+    }
+
+    
+}
+
+?>
---
+++ TI php/templates/inc/menuIncView.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<nav>
+    <ul id="top menu">
+    <li><a href="./">accueil</a></li>
+    <li><a href="./">à propos</a></li>
+    <li><a href="./">histoire</a></li>
+    <li><a href="./">contact</a></li>
+
+
+    </ul>
+</nav>
